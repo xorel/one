6678b8469992d9a775ad8b14479a0568249da888
semedi <sergio.semedi@gmail.com>
2018-01-18 15:29:00 +0100

    F #1203: new azure driver started

--- src/vmm_mad/remotes/az/az_driver.rb
+++ src/vmm_mad/remotes/az/az_driver.rb
@@ -15,33 +15,49 @@
 # limitations under the License.                                             #
 # -------------------------------------------------------------------------- #
 
-ONE_LOCATION = ENV['ONE_LOCATION'] if !defined?(ONE_LOCATION)
+ONE_LOCATION = ENV['ONE_LOCATION']
 
 if !ONE_LOCATION
-    RUBY_LIB_LOCATION = '/usr/lib/one/ruby' if !defined?(RUBY_LIB_LOCATION)
-    GEMS_LOCATION     = '/usr/share/one/gems' if !defined?(GEMS_LOCATION)
-    ETC_LOCATION      = '/etc/one/' if !defined?(ETC_LOCATION)
+    RUBY_LIB_LOCATION = '/usr/lib/one/ruby'
+    GEMS_LOCATION     = '/usr/share/one/gems'
+    ETC_LOCATION      = '/etc/one/'
 else
-    RUBY_LIB_LOCATION = ONE_LOCATION + '/lib/ruby' if !defined?(RUBY_LIB_LOCATION)
-    GEMS_LOCATION     = ONE_LOCATION + '/share/gems' if !defined?(GEMS_LOCATION)
-    ETC_LOCATION      = ONE_LOCATION + '/etc/' if !defined?(ETC_LOCATION)
+    RUBY_LIB_LOCATION = ONE_LOCATION + '/lib/ruby'
+    GEMS_LOCATION     = ONE_LOCATION + '/share/gems'
+    ETC_LOCATION      = ONE_LOCATION + '/etc/'
 end
-
-AZ_DRIVER_CONF = "#{ETC_LOCATION}/az_driver.conf"
-AZ_DRIVER_DEFAULT = "#{ETC_LOCATION}/az_driver.default"
 
 if File.directory?(GEMS_LOCATION)
     Gem.use_paths(GEMS_LOCATION)
 end
 
+$LOAD_PATH << LIB_LOCATION + '/ruby'
+$LOAD_PATH << LIB_LOCATION + '/ruby/az_driver'
 $LOAD_PATH << RUBY_LIB_LOCATION
 
+AZ_DRIVER_CONF = "#{ETC_LOCATION}/az_driver.conf"
+AZ_DRIVER_DEFAULT = "#{ETC_LOCATION}/az_driver.default"
+
 # Load Azure credentials and environment
+require 'azure'
+require 'azure_mgmt_resources'
+require 'azure_mgmt_network'
+require 'azure_mgmt_storage'
+require 'azure_mgmt_compute'
+
+
+# Azure library
+require 'az_client'
+require 'az_helper'
+require 'az_rgroup'
+
+
+# Util lib
 require 'yaml'
 require 'rubygems'
-require 'azure'
 require 'uri'
 require 'tempfile'
+
 
 require 'CommandManager'
 require 'scripts_common'
