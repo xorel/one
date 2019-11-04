5ad12cbd507a1e904f80db87cc26d0c8c4d6d184
Jan Orel <jorel@opennebula.systems>
2019-11-04 15:14:56 +0100

    review qcow2/snap__.ssh

--- install.sh
+++ install.sh
@@ -1292,7 +1292,9 @@ TM_QCOW2_FILES="src/tm_mad/qcow2/clone \
                  src/tm_mad/qcow2/mvds \
                  src/tm_mad/qcow2/mvds.ssh \
                  src/tm_mad/qcow2/snap_create \
+                 src/tm_mad/qcow2/snap_create.ssh \
                  src/tm_mad/qcow2/snap_create_live \
+                 src/tm_mad/qcow2/snap_create_live.ssh \
                  src/tm_mad/qcow2/snap_delete \
                  src/tm_mad/qcow2/snap_revert \
                  src/tm_mad/qcow2/cpds \
